84c87cdf4005079ad6f7e65a41c384e75290aad7
binyamT <binyampowerhc@gmail.com>
2023-07-11 17:25:32 +0300

    add calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function Calculator() {
+  const assignValue = (text) => (
+    <input type="button" className="val" value={`${text}`} />
+  );
+  return (
+    <div className="container">
+      <div className="calculator">
+        <form>
+          <div className="rowValue topinp">
+            <input className="input-value" type="text" placeholder="0" />
+          </div>
+
+          <div className="rowValue">
+            {assignValue('AC')}
+            {assignValue('+/-')}
+            {assignValue('%')}
+            <input className="val opp" type="button" value="÷" />
+          </div>
+
+          <div className="rowValue">
+            {assignValue('7')}
+            {assignValue('8')}
+            {assignValue('9')}
+
+            <input className="val opp" type="button" value="x" />
+          </div>
+
+          <div className="rowValue">
+            {assignValue('4')}
+            {assignValue('5')}
+            {assignValue('6')}
+            <input className="val opp" type="button" value="-" />
+          </div>
+
+          <div className="rowValue">
+            {assignValue('1')}
+            {assignValue('2')}
+            {assignValue('3')}
+            <input className="val opp" type="button" value="+" />
+          </div>
+
+          <div className="rowValue">
+            <input className="val zero" type="button" value="0" />
+            {assignValue('.')}
+            <input className="val opp" type="button" value="=" />
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
