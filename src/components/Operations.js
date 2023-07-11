b75ff06d1877291f9180bda3d1cc61073b8c2df6
binyamT <binyampowerhc@gmail.com>
2023-07-11 19:05:09 +0300

    added one chile component for the calaculator

---
+++ src/components/Operations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Operator() {
+  return (
+    <div className="rowValue topinp">
+      <input className="input-value" type="text" placeholder="0" />
+    </div>
+  );
+}
+
+export default Operator;
