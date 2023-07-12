97b69e8c8ef4683fb118ab3a8ee801323ad8cc73
binyamT <binyampowerhc@gmail.com>
2023-07-12 17:07:42 +0300

    add operation logic for the calculator

---
+++ src/components/GetInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InputFiled() {
+  return (
+    <div className="rowValue topinp">
+      <input className="input-value" type="text" placeholder="0" value />
+    </div>
+  );
+}
+
+export default InputFiled;
