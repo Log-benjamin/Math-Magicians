ddaaaa8c011b9d260edb21669be1cae15106862d
binyamT <binyampowerhc@gmail.com>
2023-07-13 18:08:22 +0300

    add styling and include quote file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Calculator from './components/Calculator';
+import Quotes from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quotes />
     </div>
   );
 }
